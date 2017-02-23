2b61c8f589bc8981f9aa64241faac267f7dc60b5
Ray Hendricks <rayjhendricks22@gmail.com>
2017-02-23 14:27:59 -0500

    added comparison if thens

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,25 @@
-console.log("JS file is connected to HTML! Woo!")
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+    alert("You found a match!");
+}
+else {
+    alert("Sorry, try again!");
+}
+
+if (cardOne === cardTwo) {
+    alert("You found a match!");
+}
+else {
+    alert("Sorry, try again!");
+}
+
+if (cardThree === cardFour) {
+    alert("You found a match!");
+}
+else {
+    alert("Sorry, try again!");
+}
