5e101a8198ed5bedb00ad9c08bfcab26daafadd1
Ray Hendricks <rayjhendricks22@gmail.com>
2017-02-26 20:52:30 -0500

    finished game, added option for many cards but flip img doesn't work

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,36 +1,28 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-/*
-if (cardTwo === cardFour) {
-    alert("You found a match!");
-}
-else {
-    alert("Sorry, try again!");
-}
-
-if (cardOne === cardTwo) {
-    alert("You found a match!");
-}
-else {
-    alert("Sorry, try again!");
-}
-
-if (cardThree === cardFour) {
-    alert("You found a match!");
-}
-else {
-    alert("Sorry, try again!");
-}
-*/
+var cardsInPlay = [];
+// will keep track of found cards
+var pairsFound = 0;
 // select the game board ID
 var gameboard = document.getElementById('game-board');
 //ask user how many cards to play with
 var cardsNumber = prompt('How many cards?');
 //for now we are using 4 cards only
-cardsNumber = 4;
 //create cards up to cardsNumber with loop
+var cards = 0;
+if (cardsNumber == 4) {
+        cards = ["king", "king", "queen", "queen"];
+} else if (cardsNumber == 6) {
+        cards = ["king", "king", "queen", "queen", "jack", "jack"];
+} else if (cardsNumber == 8) {
+        cards = ["king", "king", "queen", "queen", "jack", "jack", "ten", "ten"];
+} else if (cardsNumber == 10) {
+        cards = ["king", "king", "queen", "queen", "jack", "jack", "ten", "ten", "nine", "nine"];
+} else if (cardsNumber == 12) {
+        cards = ["king", "king", "queen", "queen", "jack", "jack", "ten", "ten", "nine", "nine", "eight", "eight"];
+} else {
+    alert("input must be an even number between 4 and 12!");
+}
+
+console.log(cards);
 function createCards(x) {
     for (var i = 0; i < x; i++) {
     var newCard = document.createElement('div');
@@ -39,3 +31,45 @@ function createCards(x) {
     }
 }
 createCards(cardsNumber);
+
+function isTwoCards () {
+    cardsInPlay.push(this.getAttribute('data-card'));
+    if (cardsInPlay.length === 2) {
+        isMatch(cardsInPlay);
+        cardsInPlay = [];
+    }
+}
+
+function createBoard () {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.getElementsByClassName('card')[i];
+        cardElement.setAttribute('data-card', cards[i]);
+        cardElement.addEventListener('click', isTwoCards);
+        cardElement.addEventListener('click', cardImg(i));
+    }
+}
+
+function cardImg(x) {
+    var card = document.getElementsByClassName('card')[x].getAttribute;
+    console.log(card)
+    if (card = 'king') {
+        document.getElementsByClassName('card').innerHTML = '<img src="kingH.png" alt = "King of Hearts" />';
+        console.log("king");
+    } else {
+        document.getElementsByClassName('card').innerHTML = '<img src="queenH.png" alt = "Queen of Hearts" />';
+        console.log("queen");
+    }
+}
+createBoard ();
+
+function isMatch(a) {
+    if (a[0] === a[1]) {
+        document.getElementById('message').textContent = 'You found a match!';
+        pairsFound = pairsFound + 1;
+    } else {
+        document.getElementById('message').textContent = 'Sorry, try again!';
+    }
+    if (pairsFound === cards.length / 2) {
+        alert("you win!");
+    }
+}
